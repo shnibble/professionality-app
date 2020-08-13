cd32e9d7d67ebe9296a0a720b463b7852414d991
Joshua Patterson <joshualpatterson@gmail.com>
2020-08-13 15:07:15 -0400

    Added pagination to calendar events.

--- src/components/calendar/events.js
+++ src/components/calendar/events.js
@@ -37,29 +37,87 @@ const TableIcon = styled.img`
     width: 20px;
     height: 20px;
 `
+const Pager = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+`
+const PagerButton = styled.button`
+    background: none;
+    padding: 5px 15px;
+    color: #999;
+    border: none;
+    cursor: pointer;
+    transition: all .25s ease;
+
+    &:hover, &:focus {
+        outline: none;
+        color: #f88000;
+    }
+
+    &:disabled {
+        cursor: default;
+        color: #f2f2f2;
+    }
+`
+const PagerText = styled.span`
+    color: #999;
+`
 
 class Events extends React.Component {
     state = {
         error: false,
         loading: true,
         loadedWithUser: false,
-        events: []
+        events: [],
+        limit: 20,
+        offset: 0,
+        count: 0
     }
 
     loadData = () => {
         const user = this.context
         axios.get('https://professionality-api.com/calendar/get', {
             params: {
-                discord_user_id: user.discord_user_id || null
+                discord_user_id: user.discord_user_id || null,
+                limit: this.state.limit,
+                offset: this.state.offset
             }
         })
         .then(result => {
-            const events = result.data
-            this.setState({ loading: false, events, loadedWithUser: user.logged_in })
+
+            axios.get('https://professionality-api.com/calendar/get', {
+                params: {
+                    discord_user_id: user.discord_user_id || null
+                }
+            })
+            .then(total_results => {
+                const count = total_results.data.length
+                const events = result.data
+                this.setState({ loading: false, events, loadedWithUser: user.logged_in, count })
+            })
         })
         .catch(error => {
             window.alert('Issue loading events, please try refreshing the page.')
         })
+    }
+
+    previousPage = async () => {
+        let offset = this.state.offset - this.state.limit
+        if (offset < 0) {
+            offset = 0
+        }
+        await this.setState({ offset })
+        this.loadData()
+    }
+
+    nextPage = async () => {
+        let offset = this.state.offset + this.state.limit
+        if (offset < this.state.count) {
+            await this.setState({ offset: this.state.offset + this.state.limit })
+            this.loadData()
+        }
     }
 
     componentDidUpdate() {
@@ -74,6 +132,12 @@ class Events extends React.Component {
     }
 
     render() {
+        let min = this.state.offset + 1
+        let max = this.state.offset + this.state.limit
+        if (max > this.state.count) {
+            max = this.state.count
+        }
+
         return (
             <UserContext.Consumer>
                 {user => (
@@ -94,50 +158,62 @@ class Events extends React.Component {
                                 :
                                 null
                                 }
-                                <TableWrapper>
-                                    <Table>
-                                        <thead>
-                                            <tr>
-                                                <th>Date</th>
-                                                <th>Event</th>
-                                                <th title='Casters'><TableIcon src={CasterIcon} /></th>
-                                                <th title='Fighters'><TableIcon src={FighterIcon} /></th>
-                                                <th title='Healers'><TableIcon src={HealerIcon} /></th>
-                                                <th title='Tanks'><TableIcon src={TankIcon} /></th>
-                                                <th>Signed Up</th>
-                                                <th>Called Out</th>
-                                                {(user.logged_in)
-                                                ?
-                                                <th>Actions</th>
-                                                :
-                                                null
-                                                }
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {(this.state.events.map(event => (
-                                            <tr key={`event_id_${event.id}`}>
-                                                <td><NavLink to={`/event/${event.id}`}>{Moment(event.start).format('ddd M/D @ h:mm a')}</NavLink></td>
-                                                <td><NavLink to={`/event/${event.id}`}>{event.title}</NavLink></td>
-                                                <td>{event.total_casters}</td>
-                                                <td>{event.total_fighters}</td>
-                                                <td>{event.total_healers}</td>
-                                                <td>{event.total_tanks}</td>
-                                                <td>{event.total_sign_ups}</td>
-                                                <td>{event.total_call_outs}</td>
-                                                {(user.logged_in && new Date(event.start) > new Date())
-                                                ?
-                                                <td>
-                                                    <AttendanceModule event={event} loadDataFunction={this.loadData} />
-                                                </td>
-                                                :
-                                                null
-                                                }
-                                            </tr>
-                                            )))}            
-                                        </tbody>
-                                    </Table>
-                                </TableWrapper>
+                                <>
+                                    <Pager>
+                                        <PagerButton onClick={this.previousPage} disabled={(this.state.offset === 0)?true:false}>Previous</PagerButton>
+                                        <PagerText>{min} - {max} of {this.state.count}</PagerText>
+                                        <PagerButton onClick={this.nextPage} disabled={(this.state.offset >= this.state.count - this.state.limit)?true:false}>Next</PagerButton>
+                                    </Pager>
+                                    <TableWrapper>
+                                        <Table>
+                                            <thead>
+                                                <tr>
+                                                    <th>Date</th>
+                                                    <th>Event</th>
+                                                    <th title='Casters'><TableIcon src={CasterIcon} /></th>
+                                                    <th title='Fighters'><TableIcon src={FighterIcon} /></th>
+                                                    <th title='Healers'><TableIcon src={HealerIcon} /></th>
+                                                    <th title='Tanks'><TableIcon src={TankIcon} /></th>
+                                                    <th>Signed Up</th>
+                                                    <th>Called Out</th>
+                                                    {(user.logged_in)
+                                                    ?
+                                                    <th>Actions</th>
+                                                    :
+                                                    null
+                                                    }
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {(this.state.events.map(event => (
+                                                <tr key={`event_id_${event.id}`}>
+                                                    <td><NavLink to={`/event/${event.id}`}>{Moment(event.start).format('ddd M/D @ h:mm a')}</NavLink></td>
+                                                    <td><NavLink to={`/event/${event.id}`}>{event.title}</NavLink></td>
+                                                    <td>{event.total_casters}</td>
+                                                    <td>{event.total_fighters}</td>
+                                                    <td>{event.total_healers}</td>
+                                                    <td>{event.total_tanks}</td>
+                                                    <td>{event.total_sign_ups}</td>
+                                                    <td>{event.total_call_outs}</td>
+                                                    {(user.logged_in && new Date(event.start) > new Date())
+                                                    ?
+                                                    <td>
+                                                        <AttendanceModule event={event} loadDataFunction={this.loadData} />
+                                                    </td>
+                                                    :
+                                                    null
+                                                    }
+                                                </tr>
+                                                )))}            
+                                            </tbody>
+                                        </Table>
+                                    </TableWrapper>
+                                    <Pager>
+                                        <PagerButton onClick={this.previousPage} disabled={(this.state.offset === 0)?true:false}>Previous</PagerButton>
+                                        <PagerText>{min} - {max} of {this.state.count}</PagerText>
+                                        <PagerButton onClick={this.nextPage} disabled={(this.state.offset >= this.state.count - this.state.limit)?true:false}>Next</PagerButton>
+                                    </Pager>
+                                </>
                             </>
                             }
                         </Article>
--- src/components/calendar/pastEvents.js
+++ src/components/calendar/pastEvents.js
@@ -34,23 +34,81 @@ const TableIcon = styled.img`
     width: 20px;
     height: 20px;
 `
+const Pager = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+`
+const PagerButton = styled.button`
+    background: none;
+    padding: 5px 15px;
+    color: #999;
+    border: none;
+    cursor: pointer;
+    transition: all .25s ease;
+
+    &:hover, &:focus {
+        outline: none;
+        color: #f88000;
+    }
+
+    &:disabled {
+        cursor: default;
+        color: #f2f2f2;
+    }
+`
+const PagerText = styled.span`
+    color: #999;
+`
 
 class PastEvents extends React.Component {
     state = {
         error: false,
         loading: true,
         events: [],
+        limit: 20,
+        offset: 0,
+        count: 0
     }
 
     loadData = () => {
-        axios.get('https://professionality-api.com/calendar/past')
+        axios.get('https://professionality-api.com/calendar/past', {
+            params: {
+                limit: this.state.limit,
+                offset: this.state.offset
+            }
+        })
         .then(result => {
-            const events = result.data
-            this.setState({ loading: false, events })
+
+            axios.get('https://professionality-api.com/calendar/past')
+            .then(total_results => {
+                const count = total_results.data.length
+                const events = result.data
+                this.setState({ loading: false, events, count })
+            })
+            
         })
         .catch(error => {
             window.alert('Issue loading events, please try refreshing the page.')
         })
+    }
+
+    previousPage = async () => {
+        let offset = this.state.offset - this.state.limit
+        if (offset < 0) {
+            offset = 0
+        }
+        await this.setState({ offset })
+        this.loadData()
+    }
+
+    nextPage = async () => {
+        let offset = this.state.offset + this.state.limit
+        if (offset < this.state.count) {
+            await this.setState({ offset: this.state.offset + this.state.limit })
+            this.loadData()
+        }
     }
 
     componentDidMount() {
@@ -58,6 +116,12 @@ class PastEvents extends React.Component {
     }
 
     render() {
+        let min = this.state.offset + 1
+        let max = this.state.offset + this.state.limit
+        if (max > this.state.count) {
+            max = this.state.count
+        }
+
         return (
             <Container>
                 <Article>
@@ -69,36 +133,48 @@ class PastEvents extends React.Component {
                     ?
                     <p>Loading past events...</p>
                     :
-                    <TableWrapper>
-                        <Table>
-                            <thead>
-                                <tr>
-                                    <th>Date</th>
-                                    <th>Event</th>
-                                    <th title='Casters'><TableIcon src={CasterIcon} /></th>
-                                    <th title='Fighters'><TableIcon src={FighterIcon} /></th>
-                                    <th title='Healers'><TableIcon src={HealerIcon} /></th>
-                                    <th title='Tanks'><TableIcon src={TankIcon} /></th>
-                                    <th>Signed Up</th>
-                                    <th>Called Out</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {(this.state.events.map(event => (
-                                <tr key={`event_id_${event.id}`}>
-                                    <td><NavLink to={`/event/${event.id}`}>{Moment(event.start).format('ddd M/D @ h:mm a')}</NavLink></td>
-                                    <td><NavLink to={`/event/${event.id}`}>{event.title}</NavLink></td>
-                                    <td>{event.total_casters}</td>
-                                    <td>{event.total_fighters}</td>
-                                    <td>{event.total_healers}</td>
-                                    <td>{event.total_tanks}</td>
-                                    <td>{event.total_sign_ups}</td>
-                                    <td>{event.total_call_outs}</td>
-                                </tr>
-                                )))}            
-                            </tbody>
-                        </Table>
-                    </TableWrapper>
+                    <>
+                        <Pager>
+                            <PagerButton onClick={this.previousPage} disabled={(this.state.offset === 0)?true:false}>Previous</PagerButton>
+                            <PagerText>{min} - {max} of {this.state.count}</PagerText>
+                            <PagerButton onClick={this.nextPage} disabled={(this.state.offset >= this.state.count - this.state.limit)?true:false}>Next</PagerButton>
+                        </Pager>
+                        <TableWrapper>
+                            <Table>
+                                <thead>
+                                    <tr>
+                                        <th>Date</th>
+                                        <th>Event</th>
+                                        <th title='Casters'><TableIcon src={CasterIcon} /></th>
+                                        <th title='Fighters'><TableIcon src={FighterIcon} /></th>
+                                        <th title='Healers'><TableIcon src={HealerIcon} /></th>
+                                        <th title='Tanks'><TableIcon src={TankIcon} /></th>
+                                        <th>Signed Up</th>
+                                        <th>Called Out</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {(this.state.events.map(event => (
+                                    <tr key={`event_id_${event.id}`}>
+                                        <td><NavLink to={`/event/${event.id}`}>{Moment(event.start).format('ddd M/D @ h:mm a')}</NavLink></td>
+                                        <td><NavLink to={`/event/${event.id}`}>{event.title}</NavLink></td>
+                                        <td>{event.total_casters}</td>
+                                        <td>{event.total_fighters}</td>
+                                        <td>{event.total_healers}</td>
+                                        <td>{event.total_tanks}</td>
+                                        <td>{event.total_sign_ups}</td>
+                                        <td>{event.total_call_outs}</td>
+                                    </tr>
+                                    )))}            
+                                </tbody>
+                            </Table>
+                        </TableWrapper>
+                        <Pager>
+                            <PagerButton onClick={this.previousPage} disabled={(this.state.offset === 0)?true:false}>Previous</PagerButton>
+                            <PagerText>{min} - {max} of {this.state.count}</PagerText>
+                            <PagerButton onClick={this.nextPage} disabled={(this.state.offset >= this.state.count - this.state.limit)?true:false}>Next</PagerButton>
+                        </Pager>
+                    </>
                     }
                 </Article>
             </Container>
